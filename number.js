bca340328a8ed245b69af386dcc01e5f3cd431c2
unknown <josh-pye@live.nmit.ac.nz>
2024-03-07 14:26:27 +1300

    number.js

--- number.js
+++ number.js
@@ -1,9 +1,13 @@
 //NOTE - 
-/*const is constant and unchangeable, let is let it be value of and can be changed */
-const num1 = 10
-console.log(num1)
+/*const is constant and unchangeable, let is let it be value of and can be changed
+
+*/
+const pi = 3.14
+//pi = 15
+console.log(pi)
 
 let num2 = 20
+num2=25
 console.log(num2)
 
 var num3 = 30
