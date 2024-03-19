f71adfedca5cd26928eb27f780a483f5dc1c4569
unknown <josh-pye@live.nmit.ac.nz>
2024-03-19 14:31:28 +1300

    functions-2

--- functions-2.js
+++ functions-2.js
@@ -1,30 +1,30 @@
-//Create a function that takes an array and return types of elements (data types) in a new array
-//ex: arrTypes(1,3, "Ali", []) -> ["number", "number", "string","object"]
+// //Create a function that takes an array and return types of elements (data types) in a new array
+// //ex: arrTypes(1,3, "Ali", []) -> ["number", "number", "string","object"]
 
-function arrTypes() {
-    let newArray = []
-    for(let i = 0; i<arrTypes.length, i++) {
-        newArray[i] = typeof(arr[i])
-    }
-    return newArray
-}
-console.log(1,3, "Ali", [])
-//ES6
-const arrTypesNew = arr => arr.map(x => typeof x) 
-//.map() creates a new array by calling a function on every element of the original array and storing results in a new array.
+// function arrTypes() {
+//     let newArray = []
+//     for(let i = 0; i<arrTypes.length, i++) {
+//         newArray[i] = typeof(arr[i])
+//     }
+//     return newArray
+// }
+// console.log(1,3, "Ali", [])
+// //ES6
+// const arrTypesNew = arr => arr.map(x => typeof x) 
+// //.map() creates a new array by calling a function on every element of the original array and storing results in a new array.
 
-//Create a function that takes 2-D arrays and returns the sum on min values in each row
-/*
-Ex: sumMinArray(
-    [1,2,3,4,5],
-    [6,7,8,9,10],
-    [11,12,13,14,15]
-)
-output -> 1+6+11 = 18
-*/
+// //Create a function that takes 2-D arrays and returns the sum on min values in each row
+// /*
+// Ex: sumMinArray(
+//     [1,2,3,4,5],
+//     [6,7,8,9,10],
+//     [11,12,13,14,15]
+// )
+// output -> 1+6+11 = 18
+// */
 
 function sumMinArrays(arr) {
-    let sum = 0;
+    let sumTotal = 0;
 
     for(let i = 0; i < arr.length; i++) {
         let y = arr[i][0]
@@ -34,8 +34,14 @@ function sumMinArrays(arr) {
     }
     
         }
+        sumTotal += y
+        //sumTotal = sum + y
     }
-    sum += y
-    //sumTotal = sum + y
+ 
+    return sumTotal
 }
-console.log(1+6+11 = 18)
+console.log(sumMinArrays([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15]
+])); // Output: 1 + 6 + 11 = 18
