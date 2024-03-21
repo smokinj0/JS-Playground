afb428c7a624cb3534214dafb11d7e050cad9cfb
unknown <josh-pye@live.nmit.ac.nz>
2024-03-21 14:57:33 +1300

    functions-2.js

--- functions-2.js
+++ functions-2.js
@@ -110,15 +110,66 @@
 
 
 
-//Create a function that takes an array of numbers or letters and return a string
-//arrayToString([1,2,3,4,5]) -> "12345"
+// //Create a function that takes an array of numbers or letters and return a string
+// //arrayToString([1,2,3,4,5]) -> "12345"
 
-function arrayToString(arr) {
-    let result = ""
-    for(let i=0; i < arrayToString.length; i++) {
-        result = result + arr[i]
+// // function arrayToString(arr) {
+// //     let result = ""
+// //     for(let i =0; i < arr.length; i++) {
+// //         result = result + arr[i]
+// //     }
+// //     return result 
+// // }
+
+// // console.log(arrayToString([1,2,3,4,5]))
+
+
+// //create a function that takes an array with numbers transform that array into a mirror.
+// //arrayToMirror([0,2,4,6,8]) output -> [0,2,4,6,8,6,4,2,0]
+// //ES5
+// function arrayToMirror(arr) {
+//     for(let i = arr.length-2; i>=0; i--) {
+//         arr.push(arr[i])
+//     }
+//     return arr
+// }
+// console.log(arrayToMirror([0,2,4,6,8]))
+// //ES6
+// const toMirror = arr => [...arr, ...arr.reverse().slice(1)]
+// console.log(toMirror([0,2,4,6,8]))
+
+// //Creata a function that returns the total number of arrays inside a given main array
+// //totalOfArrays([[1,2,3],[4,5,6] ,10 []) -> total number of arrays = 3
+
+// function totalOfArrays(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {  //Array is object
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// console.log(totalOfArrays([[1, 2, 3], [4, 5, 6] , 10,[]])); 
+
+//Create a functionthat takes a number as an argument and returns an array of numbers
+//counting down from the given number to zero
+
+//Ex: arrayCountDown(5) output -> [5,4,3,2,1,0]
+
+function arrayCountDown(num) {
+    let array = []
+    for(let i = num; i >=0; i--) {
+        array.push(i)
+    
     }
-    return result 
+    return array
 }
+console.log(arrayCountDown(5))
 
-console.log(arrayToString[1,2,3,4,5])
+//ES6
+const countDownArray = (num) => Array.from(Array(num+1).keys()).reverse()
+console.log(countDownArray(10))
+
